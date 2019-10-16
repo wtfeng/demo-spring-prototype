ecb88f761778763ff8e720a04f407d976f7295e7
Vincent Feng <vincent.feng@fwd.com>
2019-10-16 14:48:51 +0800

    add test for comparing field directly.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 @RunWith(SpringRunner.class)
@@ -14,6 +15,12 @@ public class DemoApplicationTests {
 
     @Autowired
     AService aService;
+
+
+    @Test
+    public void should_be_the_same_instance_if_not_using_get_mehtod() {
+        assertEquals(aService.bService, aService.bService);
+    }
 
     @Test
     public void should_lookup_introduce_a_new_instance() {
@@ -24,7 +31,6 @@ public class DemoApplicationTests {
     @Test
     public void should_target_class_introduce_a_new_instance() {
         assertNotEquals(aService.getcService(), aService.getcService());
-
     }
 
 }
